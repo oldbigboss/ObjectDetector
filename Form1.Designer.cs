f0356e3f16cbd378d712d4443588ac6650e6184c
Alex <37455352+oldbigboss@users.noreply.github.com>
2021-11-30 10:06:06 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace ObjectDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 143);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(641, 625);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(48, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(589, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ваше изображение:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(892, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(519, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выбор нового изображения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(769, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(249, 72);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Google Cloud Vision";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1096, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(239, 71);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Еще кто то";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(769, 451);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1504, 804);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Определение объекта на изображении";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;  
+using System.IO;
+using System.Security;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Vision.V1;
+using Aspose.Imaging.Cloud.Sdk.Api;
+using Aspose.Imaging.Cloud.Sdk.Model;
+using Aspose.Imaging.Cloud.Sdk.Model.Requests;
+
+namespace ObjectDetector
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:\\credits\\tough-flames-333022-0da9508cffee.json");
+
+        }
+        private string SelectedImagePath;
+        
+        private void SelectImg()
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            // image filters  
+            open.Filter = "Файлы изображений(*.png; *.jpg; *.jpeg; *.gif; *.bmp)| *.png; *.jpg; *.jpeg; *.gif; *.bmp";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+
+                SelectedImagePath = open.FileName;
+            }
+            pictureBox1.Image = System.Drawing.Image.FromFile(SelectedImagePath);
+        }  
+  
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SelectImg();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // google unpaid
+        {
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            Google.Cloud.Vision.V1.Image ImgSelected = Google.Cloud.Vision.V1.Image.FromFile(SelectedImagePath);
+            IReadOnlyList<EntityAnnotation> labels = client.DetectLabels(ImgSelected);
+            string s1 = "";
+            foreach (EntityAnnotation label in labels)
+            {
+                s1+= $"Вероятность: {(int)(label.Score * 100)}%; Описание: {label.Description}/n";
+            }
+            label2.Text = s1;
+        }
+        public void BoundsAnImageFromRequestBody() // aspose
+        {
+            // Input formats could be one of the following:
+            // bmp, jpeg, and jpeg2000 
+            
+            using (FileStream inputImageStream = File.OpenRead(SelectedImagePath))
+            {
+                string method = "ssd";
+                int threshold = 50;
+                bool includeLabel = true;
+                bool includeScore = true;
+                string outPath = null;
+                string storage = null; // We are using default Cloud Storage
+
+                var request = new CreateObjectBoundsRequest(inputImageStream, method, threshold, includeLabel, includeScore, outPath, storage);
+
+                // DetectedObjectList detectedObjectList = this.ImagingApi.GetObjectBounds(request);
+                // Aspose.Imaging.Cloud.Sdk.Api.ImagingApi.Eq
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
